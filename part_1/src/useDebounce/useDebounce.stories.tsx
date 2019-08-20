1cb68ca6d4bfd536f55d01239165ed17fec25b3d
Henrikas Kuzmickas <henrikask@wix.com>
2019-08-20 10:44:37 +0300

    Add useDebounce example

---
+++ part_1/src/useDebounce/useDebounce.stories.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable import/first */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { UseDebounce } from './';
+
+storiesOf('Hoks', module).add('3. custom Hook', () => <UseDebounce />);
